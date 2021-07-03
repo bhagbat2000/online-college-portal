a7329abedfc97c2e938b88ff7213c7c0ad972762
bhagbat2000 <66985152+bhagbat2000@users.noreply.github.com>
2021-07-03 21:21:40 +0530

    Add files via upload

---
+++ info.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+  <title>Home</title>
+</head>
+<body>
+<section class="text-gray-500 bg-gray-900 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-col text-center w-full mb-20">
+      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">Master Cleanse Reliac Heirloom</h1>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom.</p>
+    </div>
+    <div class="flex flex-wrap -m-4">
+      <div class="lg:w-1/3 sm:w-1/2 p-20">
+        <div class="flex relative">
+          <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="bh1.jpeg">
+          <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+            <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">18CSE092</h2>
+            <h1 class="title-font text-lg font-medium text-white mb-3">Bhagbat Prasad Mishra</h1>
+            <p class="leading-relaxed">Student Of GIET University Pursuing BTech(2018-2022)</p>
+          </div>
+        </div>
+      </div>
+      <div class="lg:w-1/3 sm:w-1/2 p-20">
+        <div class="flex relative">
+          <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="sar.jpeg">
+          <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+            <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">18CSE097</h2>
+            <h1 class="title-font text-lg font-medium text-white mb-3">Sarbajit Jena</h1>
+            <p class="leading-relaxed">Student Of GIET University Pursuing BTech(2018-2022)</p>
+          </div>
+        </div>
+      </div>
+      <div class="lg:w-1/3 sm:w-1/2 p-20">
+        <div class="flex relative">
+          <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="jash.jpeg">
+          <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+            <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">18CSE093</h2>
+            <h1 class="title-font text-lg font-medium text-white mb-3">Jashwant Pradhan</h1>
+            <p class="leading-relaxed">Student Of GIET University Pursuing BTech(2018-2022)</p>
+          </div>
+        </div>
+      </div>
+      <div class="lg:w-1/3 sm:w-1/2 p-20">
+        <div class="flex relative">
+          <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="srp.jpeg">
+          <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+            <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">18CSE092</h2>
+            <h1 class="title-font text-lg font-medium text-white mb-3">Soumya Ranjan Pasayat</h1>
+            <p class="leading-relaxed">Student Of GIET University Pursuing BTech(2018-2022)</p>
+          </div>
+        </div>
+      </div>
+     
+    </div>
+  </div>
+</section>
+  
+</body>
+</html>
